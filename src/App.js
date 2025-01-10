0ffcae9b3018f656602eccf9b0ca425f0ca77a60
Ngoc <ngoc.held9@gmail.com>
2025-01-10 17:13:38 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
